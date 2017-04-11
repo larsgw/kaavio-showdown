9390747fcea6b19b3185fb376372801ee2676e4a
Jacob Windsor <jacob.windsor@ymail.com>
2017-04-11 13:35:34 +0200

    Add resets in highlightOff

--- src/extensions/highlightOff.js
+++ src/extensions/highlightOff.js
@@ -1,5 +1,9 @@
 import { manipulatorExtensionGenerator } from '../generators/manipulationExtension';
-import { commaDelimitedStringToArrayLikeString } from '../../utils/parseCommaDelimitedString';
+import {
+  commaDelimitedStringToArray,
+  commaDelimitedStringToArrayLikeString
+} from '../../utils/parseCommaDelimitedString';
+import { generateObjectString } from '../../utils/generateObjectString';
 
 export const highlightOff = manipulatorExtensionGenerator(
   'highlightOff',
@@ -7,6 +11,24 @@ export const highlightOff = manipulatorExtensionGenerator(
     {
       function: commaDelimitedStringToArrayLikeString,
       optional: false,
+    },
+    {
+      function: input => {
+        const arr = commaDelimitedStringToArray(input);
+        if (arr.length === 0) return ''; // Just stick with defaults if no resets
+        const toReturn = {};
+        if (arr.indexOf('others') > -1) {
+          toReturn.others = true;
+        }
+        if (arr.indexOf('hidden') > -1) {
+          toReturn.hidden = true;
+        }
+        if (arr.indexOf('panZoom') > -1) {
+          toReturn.panZoom = true;
+        }
+        return generateObjectString(toReturn);
+      },
+      optional: true,
     },
   ]
 );
